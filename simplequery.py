1c80d469c84187d2a8a7a593f65d26c1ad9e2626
Ian Barber <ian.barber@gmail.com>
2011-03-09 21:58:25 +0000

    Initial checkin

--- simplequery.py
+++ simplequery.py
@@ -2,7 +2,7 @@ from solr import *
 url = 'http://localhost:8080/solr/main'
 s = SolrConnection(url)
 
-response = s.query('idie')
+response = s.query('idie manager')
 for hit in response.results:
     print hit['title']
     print hit['body']
